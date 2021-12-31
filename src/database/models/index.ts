9867d330288c23d4a3e33951941c03ac34cd42dd
Jan Sobanski <sobanski.jan2@gmail.com>
2021-12-31 05:35:31 +0100

    add facebook user model

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,7 +1,8 @@
 import User from './User'
 import GithubUser from './GithubUser'
 import GoogleUser from './GoogleUser'
+import FacebookUser from './FacebookUser'
 
 export { 
-  User, GithubUser, GoogleUser
+  User, GithubUser, GoogleUser, FacebookUser
 }
