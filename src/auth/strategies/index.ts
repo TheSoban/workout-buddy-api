68e8f407f267436d4b69ad84b40bfb0ebc54835e
Jan Sobanski <sobanski.jan2@gmail.com>
2021-12-31 06:31:23 +0100

    add local strategy

--- src/auth/strategies/index.ts
+++ src/auth/strategies/index.ts
@@ -1,5 +1,6 @@
 import githubStrategy from './githubStrategy'
 import googleStrategy from './googleStrategy'
 import facebookStrategy from './facebookStrategy'
+import localStrategy from './localStrategy'
 
-export { githubStrategy, googleStrategy, facebookStrategy }
+export { githubStrategy, googleStrategy, facebookStrategy, localStrategy }
